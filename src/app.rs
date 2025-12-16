8213f32d9c056a20a0c38e362aeb3297b5a65c37
Antonio Yang <yanganto@gmail.com>
2025-12-16 13:51:56 +0800

    show case on using dispatch method

--- src/app.rs
+++ src/app.rs
@@ -12,11 +12,20 @@ impl App {
 
     /// Handle a key event and return false if the app should quit
     pub fn handle_key(&mut self, key: KeyEvent) -> bool {
+        // You can handle your event in one of following ways
+
+        // Option 1: match_any
         if KeyBindEvent::Quit.match_any(&key) {
             return false;
         }
-        // Add more keybinding matches here as you extend the application
-        true
+
+        // Option 2 match events from dispatch method
+        for event in KeyBindEvent::dispatch(&key) {
+            match event {
+                KeyBindEvent::Quit => return false,
+            }
+        }
+        false
     }
 }
 
